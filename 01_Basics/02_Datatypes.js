e09c470f47434d58fb18860d5ff0c1909adbe4c2
Himanshu Meshram <148863857+HimanshuMesheram@users.noreply.github.com>
2024-09-27 06:38:40 +0000

    added datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+let name = "Himanshu"
+
+let age = 23
+
+let isAdult = true
+
+console.log(name);
+
+
+// number
+//bigint
+//string : ""
+//boolean : true/false
+//null : standalone value
+//undefined : value yet not assigned
+//symbol : uniqueness
+
+console.log(typeof undefined);
+
+console.log(typeof null);
